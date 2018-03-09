4884faafd433e49f2bfa051da49ff30bab2d84ac
pitapruebag <smis117111@gmail.com>
2018-03-08 23:45:04 -0600

    qwertyu

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <title>PYTHAGORAS</title>
     <!-- Place favicon.ico in the root directory -->
     <link rel="apple-touch-icon" href="logo1.png">
-    <link rel="shortcut icon" type="image/ico" href="logo1.ico" />
+    <link rel="shortcut icon" type="image/ico" href="logo1ic.ico" />
     <!-- Plugin-CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
