c0a4ecfc9998ef5e49696950caafc92e2eb0c8d9
pitapruebag <smis117111@gmail.com>
2018-04-18 17:29:55 -0600

    500
    500

--- index.php
+++ index.php
@@ -1400,7 +1400,7 @@
                 </div>-->
                 <div class="row">
                     <center><div class="col-xs-12 col-md-12">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3879.8415415915642!2d-88.17584465500892!3d13.483860006742418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f7b2a7b8cc71105%3A0x752512f15f1a1777!2sCentro+Escolar+Pablo+J.+Aguirre!5e0!3m2!1ses-419!2ssv!4v1523593791105" frameborder="0" style="border:0; width:100%; height:700px;" allowfullscreen></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3879.8415415915642!2d-88.17584465500892!3d13.483860006742418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f7b2a7b8cc71105%3A0x752512f15f1a1777!2sCentro+Escolar+Pablo+J.+Aguirre!5e0!3m2!1ses-419!2ssv!4v1523593791105" frameborder="0" style="border:0; width:100%; height:500px;" allowfullscreen></iframe>
                          <!--<address class="side-icon-boxes">
                            <div class="side-icon-box">
                                 <div class="side-icon">
