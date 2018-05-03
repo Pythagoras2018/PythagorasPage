02e842503394727a965d28451480fae73f287c7b
pitapruebag <smis117111@gmail.com>
2018-05-03 14:30:29 -0600

    asd
    asd

--- index.php
+++ index.php
@@ -1401,7 +1401,7 @@
                 <div class="row">
                     <center><div class="col-xs-12 col-md-12">
                         <h2>Esta es la ubicación del sitio donde se realizarán las clases de los cursos</h2><br>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3879.8415415915642!2d-88.17584465500892!3d13.483860006742418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f7b2a7b8cc71105%3A0x752512f15f1a1777!2sCentro+Escolar+Pablo+J.+Aguirre!5e0!3m2!1ses-419!2ssv!4v1523593791105" frameborder="0" style="border:0; width:100%; height:490px;" allowfullscreen></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1276.9433959525034!2d-88.1757991594783!3d13.482264321013279!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f7b2a7a577b3b0f%3A0x7f6528c9687dd617!2sCentro+Escolar+Sagrado+Coraz%C3%B3n!5e0!3m2!1ses-419!2ssv!4v1525379267143" frameborder="0" style="border:0; width:100%; height:490px;" allowfullscreen></iframe>
                          <!--<address class="side-icon-boxes">
                            <div class="side-icon-box">
                                 <div class="side-icon">
